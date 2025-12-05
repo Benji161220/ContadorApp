aa2e701993f5a5c8cb3b017335f8262ab17fab40
benvarzam <benje1612@gmail.com>
2025-12-05 20:39:30 +0100

    Centrado

--- app/src/main/java/com/example/contadorapp/ui/ContadorScreen.kt
+++ app/src/main/java/com/example/contadorapp/ui/ContadorScreen.kt
@@ -4,23 +4,31 @@ package com.example.contadorapp.ui
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.contadorapp.ui.theme.ContadorAppTheme
 
 @Composable
-fun ContadorApp(
+fun ContadorApp(modifier : Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)
 ){
-    Box(
-        modifier = Modifier
-
-    ){
-        Column {
+        Column (modifier = modifier){
+            Row{
+                Text("Hola",modifier = Modifier.height(140.dp))
+            }
             Row{
                 Text("Hola")
                 Button(
@@ -39,8 +47,9 @@ fun ContadorApp(
             }
 
         }
-    }
 }
+
+
 
 @Preview
 @Composable
